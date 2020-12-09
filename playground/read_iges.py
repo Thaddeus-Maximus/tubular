cd314b278f4a72ad73632217a8dbd7ebcd66afa8
Thaddeus Hughes <hughes.thad@gmail.com>
2020-12-09 15:00:31 -0500

    Just realized this is going to be way more complex than initially thought. We'll have to break things down far. However, I can prefix decomposed members- that way they can still be referenced by a broad scope when doing material assignment.

--- playground/read_iges.py
+++ playground/read_iges.py
@@ -5,14 +5,15 @@ from iges.curves_surfaces import CircArc, Line, CompCurve, AssociativityInstance
 
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 
 fig = plt.figure()
 ax  = fig.add_subplot(111, projection='3d')
 ax.set_aspect('auto')
 
 # Load
-with open('tubes.iges', 'r') as f:
-    igs = IGES_Object(f)
+with open('tubes_dual.iges', 'r') as f:
+	igs = IGES_Object(f)
 
 # Do whatever you want with the data. Go wild.
 # igs.toplevel_entities contains the highest-level topological data.
@@ -21,9 +22,17 @@ with open('tubes.iges', 'r') as f:
 # The goal is to provide a uniform interface for all entities, though, so that it doesn't matter.
 # 
 # This will plot each topology in a unique (ish) color so you can see that entities have been grouped appropriately.
+print([type(x) for x in igs.toplevel_entities])
+
+P = np.array([.02, .01, -.1]).reshape(3, 1)
+plt.plot(P[0], P[1], P[2], 'kx')
+
 for i, entity in enumerate(igs.toplevel_entities):
-    if type(entity) in [CircArc, Line, CompCurve, AssociativityInstance]:
-        lsp = entity.linspace(10, endpoint=True)
-        plt.plot(lsp[0,:], lsp[1,:], lsp[2,:], '-*')
-        
+	if type(entity) in [CircArc, Line, CompCurve, AssociativityInstance]:
+		lsp = entity.linspace(30, endpoint=True)
+		plt.plot(lsp[0,:], lsp[1,:], lsp[2,:], '-')
+
+		dist, nearest = entity.nearestPoint(P)
+		print(dist, nearest)
+		plt.plot(nearest[0], nearest[1], nearest[2], 'ko')
 plt.show()
